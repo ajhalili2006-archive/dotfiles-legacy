d7c39177f2e9882728ce8ba77bce52f0ba91f6c4
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-02-20 22:48:14 +0800

    Shit Linus did again.

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -6,8 +6,10 @@ echo "Starting up in 3 seconds..."
 sleep 3
 
 echo "==> Prompt ahead: Enter your GitLab Auth tokens"
+
 read -p "Your username: " GH_USERNAME
-read -s -p "Your GitLab.com PAT: " GH_PAT
+
+read -sp "Your GitLab.com PAT: " GH_PAT
 
 if echo $OSTYPE | grep linux-android.*; then
     # Assuming that you ibstalled wget and curl
